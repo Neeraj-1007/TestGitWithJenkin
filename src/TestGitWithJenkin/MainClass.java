22e10f5c77e7587722ca120c0188cf2ababec0fb
Neeraj-1007 <neerajkumarjha10@gmail.com>
2022-05-20 01:02:38 +0530

    Initial Commit

---
+++ src/TestGitWithJenkin/MainClass.java
@@ -0,0 +1,10 @@
+package TestGitWithJenkin;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Master Commit");
+	}
+
+}
