be411a13d332533d59b7b62fb2d03be500c077a9
Neeraj-1007 <neerajkumarjha10@gmail.com>
2022-05-20 01:48:36 +0530

    Initial subs class commit

--- src/TestGitWithJenkin/MainClass.java
+++ src/TestGitWithJenkin/MainClass.java
@@ -4,8 +4,13 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Master Commit");
-System.out.println("Subs Commit");
+		System.out.println("Master Commit");
+		System.out.println("Subs Commit");
+
+		int i = 0;
+		SubstractionClass subs = new SubstractionClass();
+		i = subs.subs();
+		System.out.println(i);
 	}
 
 }
---
+++ src/TestGitWithJenkin/SubstractionClass.java
@@ -0,0 +1,12 @@
+package TestGitWithJenkin;
+
+public class SubstractionClass {
+	
+	public int subs() {
+		int i=0;
+		int a=8,b=7;
+		i=a-b;
+		return i;
+	}
+
+}
