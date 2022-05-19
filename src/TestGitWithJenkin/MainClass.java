5a3b4f622784e4af2b463b82adbcf6884314130f
Neeraj-1007 <neerajkumarjha10@gmail.com>
2022-05-20 01:51:49 +0530

    Initial commit for mult class

--- src/TestGitWithJenkin/MainClass.java
+++ src/TestGitWithJenkin/MainClass.java
@@ -4,8 +4,12 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Master Commit");
-System.out.println("Mult Commit");
+		System.out.println("Master Commit");
+		System.out.println("Mult Commit");
+		int i = 0;
+		MultiplicationClass mult = new MultiplicationClass();
+		i = mult.mult();
+		System.out.println(i);
 	}
 
 }
---
+++ src/TestGitWithJenkin/MultiplicationClass.java
@@ -0,0 +1,11 @@
+package TestGitWithJenkin;
+
+public class MultiplicationClass {
+
+	public int mult() {
+		int i=0;
+		int a=7,b=5;
+		i=a*b;
+		return i;
+	}
+}
