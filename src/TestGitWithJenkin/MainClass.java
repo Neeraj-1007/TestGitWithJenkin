c007fca567cf0d9d41ba9c3c4d8bb10a17900767
Neeraj-1007 <neerajkumarjha10@gmail.com>
2022-05-20 02:05:16 +0530

    initial commit for merging add into master

--- src/TestGitWithJenkin/MainClass.java
+++ src/TestGitWithJenkin/MainClass.java
@@ -4,7 +4,7 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Master Commit");
+		
 		System.out.println("Add Commit");
 		int i=0;
 		AdditionClass add=new AdditionClass();
